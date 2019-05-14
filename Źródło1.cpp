9fe0116b8ad98985545b663114546f85c90f4586
Hejsan <kamil.swiekala@gmail.com>
2019-05-14 13:47:55 +0200

    update

---
+++ Źródło1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+using namespace std;
+#define N 100
+char napis[N + 1]
+
+void pierwsza ()
+
+
+
+int main()
+{
+	cout << "Wpisz wyraz";
+	strig a; cin >> a;
+}
+
+
+
+
+
+
